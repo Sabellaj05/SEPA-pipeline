696a4d0e409adff55b533bdb83cc66ee00ad308a
macsaens <thesanes@proton.me>
2025-10-23 18:12:00 -0300

    ...

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Running black formatter"
+echo "Running black formatter..."
 uv run black src/ tests/
-echo "Running isort to sort imports"
+echo "Running isort to sort imports..."
 uv run isort src/ tests/
-echo "Running flake8 for linting"
+echo "Running flake8 for linting..."
 uv run flake8 src/ tests/
-echo "Running mypy for type cheking"
+echo "Running mypy for type cheking..."
 uv run mypy src/
-echo "Running pytest for cooking pasta"
+echo "Running pytest for cooking pasta..."
 uv run pytest
